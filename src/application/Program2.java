3d64f3abc379221bc5694721c60429be30eee33c
leeconde <lecondesouza@gmail.com>
2020-09-19 23:45:18 -0300

    Delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,6 +11,8 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
@@ -35,6 +38,14 @@ public class Program2 {
 		department.setName("Teste 4");
 		departmentDao.update(department);
 		System.out.println("Update complete!");
+		
+		System.out.println("\n=== TEST 5: department delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete complete!");
+		
+		sc.close();
 
 	}
 
